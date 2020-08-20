c854597794cc4502dbb40a081c2c4a76e232c6ca
BonHyuck <qhsgur0126@gmail.com>
2020-08-20 15:52:34 +0900

    D3에서 좀 막힌듯

---
+++ D3/5293.py
@@ -0,0 +1,11 @@
+# SWEA 5293
+# 이진 문자열 분석
+T = int(input())
+for test_case in range(1, T+1):
+    # A : “00”
+    # B : “01”
+    # C : “10”
+    # D : “11”
+    A, B, C, D = map(int, input().split())
+
+    
