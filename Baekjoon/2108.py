0c4449b37d94fd3860c44d16bc9e8a3202a5151e
BonHyuck <qhsgur0126@gmail.com>
2021-05-03 17:56:55 +0900

    통계학에서 시간초과, 해결 필수

--- Baekjoon/2108.py
+++ Baekjoon/2108.py
@@ -1,4 +1,19 @@
 N = int(input())
 arr = sorted([int(input()) for _ in range(N)])
+count_arr = [[] for _ in range(N+1)]
+for k in set(arr):
+    count_arr[arr.count(k)].append(k)
+
+result = 0
+for index in range(N, -1, -1):
+    if len(count_arr[index]) > 0:
+        if len(count_arr[index]) > 1:
+            result = sorted(count_arr[index])[1]
+        else:
+            result = sorted(count_arr[index])[0]
+        break
+
 print(sum(arr)//len(arr))
 print(arr[len(arr)//2])
+print(result)
+print(max(arr) - min(arr))
